fbb09dae814b9005ff089ba394cd48db718c8e6b
Ledaryy <andrew.ledary@gmail.com>
2022-05-15 23:16:33 +0100

    WIP adding postgresql db

--- money_bot/webhost/webhost/settings.py
+++ money_bot/webhost/webhost/settings.py
@@ -103,9 +103,13 @@ WSGI_APPLICATION = 'webhost.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgrespw",
+        "HOST": "localhost",
+        "PORT": 5432,
     }
 }
 

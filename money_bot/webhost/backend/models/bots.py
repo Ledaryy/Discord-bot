2d7fcb46a81bd51171cd57494ac5f7ca394e559f
Ledaryy <andrew.ledary@gmail.com>
2022-05-15 11:48:21 +0100

    renamed

--- money_bot/webhost/backend/models/bots.py
+++ money_bot/webhost/backend/models/bots.py
@@ -4,4 +4,4 @@ class Bot(models.Model):
     
     name = models.CharField(max_length=100)
     token = models.CharField(max_length=100)
-    balance = models.ForeignKey("backend.Money", verbose_name="bot", on_delete=models.CASCADE)
+    money = models.ForeignKey("backend.Money", verbose_name="bot", on_delete=models.CASCADE)

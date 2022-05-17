6ffbeabfed53797e17b7462780750ea856fb047a
Ledaryy <andrew.ledary@gmail.com>
2022-05-17 20:00:55 +0100

    set up deployment conf for nginx, env, added gunicorn

---
+++ money_bot/webhost/Docker/entrypoint.prod.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python manage.py migrate &&
+python manage.py collectstatic --no-input --clear &&
+
+gunicorn webhost.wsgi -w 1 -b 0.0.0.0:8000 --timeout 120 --workers 5 --worker-class gevent
--- money_bot/webhost/Docker/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python manage.py migrate &&
-python manage.py collectstatic --no-input &&
-
-gunicorn webhost.wsgi -w 1 -b 0.0.0.0:80 --timeout 120 --workers 5 --worker-class gevent

b2b38b4a4e2e16794f40b5c64091f5bee61c99fa
Ledaryy <andrew.ledary@gmail.com>
2022-05-17 23:10:36 +0100

    redirect to admin fixed

--- money_bot/webhost/webhost/urls.py
+++ money_bot/webhost/webhost/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
 from django.urls import path
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
-    path('/', admin.site.urls),
+    path('', RedirectView.as_view(url='admin/', permanent=False), name='index'),
+    path('admin/', admin.site.urls),
 ]

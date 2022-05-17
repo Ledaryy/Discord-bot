31d0e3f12aa4481a728c05da4b08e6a65f6d8515
Ledaryy <andrew.ledary@gmail.com>
2022-05-17 10:40:05 +0100

    logic for Moneylog and formatting

--- money_bot/webhost/backend/models/bots.py
+++ money_bot/webhost/backend/models/bots.py
@@ -1,15 +1,16 @@
 import celery
 from django.db import models
 
+
 class Bot(models.Model):
 
     name = models.CharField(max_length=100)
     token = models.CharField(max_length=100)
     is_active = models.BooleanField(default=False)
-    
+
     balance = models.IntegerField(default=0)
     total_earned = models.IntegerField(default=0)
-    
+
     def __str__(self):
         return self.name
 
@@ -17,6 +18,6 @@ class Bot(models.Model):
         # Starts auto collect chain
         celery.send_task("backend.tasks.start_collect", args=(self.id,))
         return True
-    
+
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
--- money_bot/webhost/backend/models/money.py
+++ money_bot/webhost/backend/models/money.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 class MoneyLog(models.Model):
-    
+
     owner = models.OneToOneField(
         "Bot",
         related_name="money",
@@ -9,6 +10,16 @@ class MoneyLog(models.Model):
     )
     earned = models.IntegerField(default=0)
     date = models.DateTimeField(auto_now_add=True)
-        
+
     def __str__(self):
         return f"{self.owner} - {self.date} - {self.earned}"
+
+    def save(self, *args, **kwargs):
+
+        # Update owner's balance
+        self.owner.balance += self.earned
+        self.owner.total_earned += self.earned
+
+        self.owner.save()
+
+        super().save(*args, **kwargs)

955863a9afb89780a6c9f4527d7aaadd16e2b624
Ledaryy <andrew.ledary@gmail.com>
2022-05-19 00:49:16 +0100

    task delete added, formatting

--- money_bot/webhost/backend/tasks.py
+++ money_bot/webhost/backend/tasks.py
@@ -1,3 +1,4 @@
+import uuid
 import logging
 
 from celery import shared_task
@@ -5,6 +6,7 @@ from .models import Bot
 from .models.bots import Bot, BotRoles
 
 from time import sleep
+from django.core.cache import cache
 from datetime import datetime, timedelta
 # from webhost.utils.discord import Discord
 
@@ -16,11 +18,11 @@ logger = logging.getLogger(__name__)
 @shared_task
 def start_bot(bot_id, delay):
     logger.info(
-        f"Started background bot {bot_id}, delay: {delay}")
+        f"Started background task, init start bot {bot_id}, delay: {delay}")
     try:
 
         eta = datetime.now() + timedelta(minutes=delay)
-
+        
         _start_bot.apply_async(
             eta=eta,
             kwargs={
@@ -31,23 +33,39 @@ def start_bot(bot_id, delay):
     except Exception as e:
         logger.error(
             f"Error {e} while starting bot {bot_id}")
-    logger.info(f"Finished background start bot task for {bot_id}")
+    logger.info(f"Finished background init start bot for {bot_id}")
 
 
 @shared_task()
 def _start_bot(bot_id: int):
-    logger.info(f"Started background collect task for bot {bot_id}")
+    
+    logger.info(f"Started background start bot task {bot_id}")
 
     bot = Bot.objects.get(id=bot_id)
     bot.is_active = True
     bot.save()
+    
+    CACHE_TEMPLATE = {
+        "bot_name": bot.name,
+        "bot_id": bot.id,
+        "started_at": datetime.now(),
+        "collecter": {
+            "active": False,
+            "next_task_id": None,
+            "next_task_eta": None,
+            "next_task_eta_seconds": None,
+        }
+    }
+    
+    cache.set(f"bot_{bot.id}", CACHE_TEMPLATE)
+    
 
     if bot.role == BotRoles.disabled:
         logger.info(f"This bot is disabled: {bot}")
     if bot.role == BotRoles.collecter:
         start_send_collect_money(bot_id)
-
-    logger.info(f"Finished background collect task for bot {bot_id}")
+        
+    logger.info(f"Finished background start bot task {bot_id}")
 
 
 @shared_task()
@@ -56,11 +74,15 @@ def start_send_collect_money(bot_id: int):
 
     bot = Bot.objects.get(id=bot_id)
     
-    BotCollecter(
-        bot=bot
-    ).collect()
-
     if bot.is_active:
+        BotCollecter(
+            bot=bot
+        ).collect()
+
+        bot_cache = cache.get(f"bot_{bot.id}")
+        bot_cache['collecter']['active'] = True
+        cache.set(f"bot_{bot.id}", bot_cache)
+        
         reshedule_send_task(bot)
     else:
         logger.info(f"Finished collect task for bot {bot}")
@@ -73,8 +95,22 @@ def reshedule_send_task(bot: Bot):
 
     delay = bot.get_collecter_delay_in_seconds()
     eta = datetime.now() + timedelta(seconds=delay)
+    task_uniq_id = uuid.uuid4()
+    task_uniq_id = str(task_uniq_id)
+    
+    bot_cache = cache.get(f"bot_{bot.id}")
+    
+    bot_cache["collecter"]["next_task_id"] = task_uniq_id
+    bot_cache["collecter"]["next_task_eta"] = eta
+    bot_cache["collecter"]["next_task_eta_seconds"] = delay
+    
+    cache.set(f"bot_{bot.id}", bot_cache)
+    
+    logger.info(f"Cache resh task: {bot_cache}")
+    
     start_send_collect_money.apply_async(
         eta=eta,
+        task_id=task_uniq_id,
         kwargs={
             "bot_id": bot.id,
         },
@@ -101,10 +137,21 @@ def stop_bot(bot_id, delay):
 
 @shared_task
 def _stop_bot(bot_id):
+    from webhost.celery import app
     logger.info(f"Started background stop task for bot {bot_id}")
     
     bot = Bot.objects.get(id=bot_id)
     bot.is_active = False
     bot.save()
+    
+    bot_cache = cache.get(f"bot_{bot.id}")
+    
+    if bot_cache['collecter']['active']:
+        app.control.revoke(bot_cache['collecter']['next_task_id'])
+        logger.info(f"Task revoked: {bot_cache['collecter']['next_task_id']}")
+    
+    logger.info(f"Cache: {bot_cache}")
+    
+    cache.delete(f"bot_{bot.id}")
 
     logger.info(f"Finished background stop task for bot {bot_id}")

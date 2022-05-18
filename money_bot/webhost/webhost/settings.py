e67058f45290948e501c134aaed28eba82fba6e6
Ledaryy <andrew.ledary@gmail.com>
2022-05-18 18:51:37 +0100

    set redis as django cache

--- money_bot/webhost/webhost/settings.py
+++ money_bot/webhost/webhost/settings.py
@@ -86,6 +86,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'webhost.wsgi.application'
 
+# Cache ========================================================================
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "main"
+    },
+}
+
 # Database =====================================================================
 
 DATABASES = {

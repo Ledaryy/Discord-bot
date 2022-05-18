67761454f37027b7a6686a973294f99de0ffbc6f
Ledaryy <andrew.ledary@gmail.com>
2022-05-19 00:50:13 +0100

    delay between messsage from 30 to 5,10

--- money_bot/webhost/backend/bots/collecter.py
+++ money_bot/webhost/backend/bots/collecter.py
@@ -25,10 +25,10 @@ class BotCollecter(Discord):
         crime = random.choice([True, False])
         if crime:
             logger.info("Crime function triggered")
-            sleep(random.randint(10, 30))
+            sleep(random.randint(5, 10))
             self.send_message(WORK_CHANNEL_ID, ",crime")
 
-        sleep(random.randint(10, 30))
+        sleep(random.randint(5, 10))
         self.send_message(WORK_CHANNEL_ID, ",collect")
 
         logger.info(f"Collect finished")

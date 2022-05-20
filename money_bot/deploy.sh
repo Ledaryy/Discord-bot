6bab86f73809a5db198a8a8d64ad71378d672727
Ledaryy <andrew.ledary@gmail.com>
2022-05-20 14:50:53 +0100

    deploy script added

---
+++ money_bot/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Deploying to the production environment"
+
+echo "Removing old containers"
+sudo docker rm -f compose_nginx_1 &&
+sudo docker rm -f compose_web_1 &&
+sudo docker rm -f compose_celery_1 &&
+sudo docker rm -f compose_redis_1 &&
+sudo docker rm -f compose_db_1 &&
+
+echo "Building new containers and starting them"
+sudo docker-compose -f ./compose/docker-compose.prod.yml up -d --build &&
+echo "Done"

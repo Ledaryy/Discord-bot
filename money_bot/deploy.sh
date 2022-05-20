3c5a7e43849c648874f90e91ec4f1f93564d02e7
Ledaryy <andrew.ledary@gmail.com>
2022-05-20 20:14:05 +0100

    deploy scirpt updated, to keep db

--- money_bot/deploy.sh
+++ money_bot/deploy.sh
@@ -7,7 +7,7 @@ sudo docker rm -f compose_nginx_1 &&
 sudo docker rm -f compose_web_1 &&
 sudo docker rm -f compose_celery_1 &&
 sudo docker rm -f compose_redis_1 &&
-sudo docker rm -f compose_db_1 &&
+sudo docker rm -f compose_flower_1 &&
 
 echo "Building new containers and starting them"
 sudo docker-compose -f ./compose/docker-compose.prod.yml up -d --build &&

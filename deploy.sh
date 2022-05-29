a370ef7d9fb62de033fabe25e990ef071698edad
Ledaryy <andrew.ledary@gmail.com>
2022-05-29 13:02:54 +0100

    license header

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#
+# Author: Andrew Kulishov <support@andrewkulishov.com>
+# Copyright (C) 2022 Andrew Kulishov - All Rights Reserved
+# 
+# Created on Sun May 29 2022
+# 
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# 
+# If there are any issues contact me on the email above.
+#
+
 
 echo "Deploying to the production environment"
 

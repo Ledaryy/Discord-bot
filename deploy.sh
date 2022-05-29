14e05d9d08c63e25e5c33372e37ad8e0d04c83e7
Ledaryy <andrew.ledary@gmail.com>
2022-05-29 13:01:37 +0100

    added git pull to deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,9 @@ sudo docker rm -f compose_celery_1 &&
 sudo docker rm -f compose_redis_1 &&
 sudo docker rm -f compose_flower_1 &&
 
+echo "Updating the code"
+sudo git pull &&
+
 echo "Building new containers and starting them"
 sudo docker-compose -f ./compose/docker-compose.prod.yml up --build &&
 echo "Done"
